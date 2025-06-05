b590a95aa0a0cd0d02c54deaf03e27a01bed2629
Chet <cmcgrury2@huskers.unl.edu>
2025-06-05 13:36:34 -0500

    Added element to display time of data collection, rain volume over 1 or 3 hours, improved look, started forecast implementation

---
+++ README.md
@@ -0,0 +1,32 @@
+# Chet's Weather App
+
+This is a project I started to teach myself how to use APIs, as well as building webpages using HTML, JS, and CSS.
+
+A simple web app that allows users to input a city name and displays:
+- Weather Description
+- Current Temperature
+- Current Feels Like Temperature
+- Wind Speed
+- Sunrise Time
+- Humidity
+- Rain Volume (if applicable)
+- Time Of Data Collection
+
+## How to use:
+1. Enter a city name (for example, 'Lincoln', 'Lincoln, NE, US')
+2. Click the 'Get my weather' button
+3. Weather data will appear below
+
+## Features to add:
+- An option to see a future forecast
+- More error handling
+- Reformat the page to have two columns instead of 1, allowing for more data to be displayed
+- General asthetic improvements, more pictures, etc.
+- More to come...
+
+## Technologies used:
+- HTML
+- CSS
+- JavaScript
+- OpenWeatherMap API
+
--- script.js
+++ script.js
@@ -11,6 +11,17 @@ document.getElementById("getWeatherButton").addEventListener("click", function()
     console.log ("City entered:", cityName);
     let apiKey = "4e8ddf8786f8126d264f95a3d70a0f72";
     let url = `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${apiKey}&units=metric`;
+    let forecastUrl = `https://api.openweathermap.org/data/2.5/forecast?q=${cityName}&appid=${apiKey}&units=metric`;
+
+    fetch(forecastUrl)
+    .then(response => response.json())
+    .then(forecastData => {
+        if (data.cod != 200) {
+            document.getElementById("weatherResult").innerHTML = '<p>City not found. Please try again.</p>'
+        }
+    })
+
+
 
 
     fetch(url) /* fetch() sends a get request (a request to get the data from that address) to OpenWeatherMap for the weather data of the selected city. The result of fetch(url) is a Promise, represents the process of the request being sent and waiting for a response. */
@@ -24,7 +35,9 @@ document.getElementById("getWeatherButton").addEventListener("click", function()
         
          /* In the future I'd like to put the following in a function to make it a bit cleaner but I'll worry about that in the future so I don't mess up my comments */
     let tempC = data.main.temp;
-    let tempF = tempC * (9/5) + 32;
+    let tempF = (tempC * (9/5) + 32).toFixed(1);
+    let tempCFeel = data.main.feels_like;
+    let tempFFeel = (tempCFeel * (9/5) + 32).toFixed(1);
     let humidity = data.main.humidity;
     let sunrise = data.sys.sunrise;
     let timezoneOffset = data.timezone;
@@ -34,16 +47,36 @@ document.getElementById("getWeatherButton").addEventListener("click", function()
     let ampm = hours >= 12 ? "PM" : "AM"; /* ampm = if hours is greater than or equal to 12 then ampm = PM, otherwise ampm = AM. */
     hours = hours % 12 || 12; /* converts 0 to 12 */
     let formatted = `${hours}:${minutes.toString().padStart(2, 0)} ${ampm}`;
+
+    let timeCollected = new Date((data.dt + timezoneOffset) * 1000); /* Display at the bottom of the page what time the weather data being displayed was collected in the local time of the city searched */
+    let timeCollectedHours = timeCollected.getUTCHours();
+    let timeCollectedMins = timeCollected.getUTCMinutes();
+    let collectedAMPM = timeCollectedHours >= 12 ? "PM" : "AM";
+    timeCollectedHours = timeCollectedHours % 12 || 12;
+    let timeCollectedFormatted = `${timeCollectedHours}:${timeCollectedMins.toString().padStart(2, 0)} ${collectedAMPM}`;
+
+    let rain = data.rain;
+    let rainVolume = "";
+
+    if (data.rain?.["1h"]) { /* Check if data.rain has data for the last hour, if so set rainVolume to that data. */
+        rainVolume = `${data.rain["1h"]} mm in the last hour`;
+    } else if (data.rain?.["3h"]) {
+        rainVolume = `${data.rain["3h"]} mm in the last 3 hours`;
+    } else {
+        rainVolume = "It hasn't rained in the last 3 hours.";
+    }
     /* "`${...}`" is a simpler way of writing "blah blah blah" + x + "blah blah blah" + y ...   So it is actually displayed here as: hours + ":" + minutes + "AM/PM". .padStart(x,y) only works with strings (thats why we used toString() on minutes). padStart then ensures that: when the minutes string is less than 2 characters, add a leading zero. This ensures that it displays as 7:05 instead of 7:5 for example. */
 
          document.getElementById("weatherResult").innerHTML = ` 
          <h2>${data.name}, ${data.sys.country}</h2> 
-         <p> Temp: ${tempF}° F </p>
          <p> Weather: ${data.weather[0].description}</p>
-         <p> Feels Like: ${data.main.feels_like} C </p>
+         <p> Temp: ${tempF}° F </p>
+         <p> Feels Like: ${tempFFeel}° F </p>
          <p> Wind Speed: ${data.wind.speed} MPH </p>
          <p> Sunrise: ${formatted}</p>
          <p> Humidity: ${humidity}%</p>
+         <p> Rain: ${rainVolume}</p>
+         <h6> Time of data collection (in city's local time): ${timeCollectedFormatted}</h6>
          `; /* <h2> is a level of heading, it goes from <h1> (main title) to <h6> (tiniest heading). from my understanding, .innerHTML in this case takes weatherResult (from document.getElementByID) and when the script runs changes it to what is in the ``. Also, the data.x keys that display different data can be found on the API site. */
         
     })
